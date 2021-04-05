7b12189a7dcead405b75f62c8a104741c9fe3364
Michael Maguire <michaelqmaguire2@ufl.edu>
2021-04-05 14:18:00 -0400

    script to create directories

---
+++ 01_create-directories.R
@@ -0,0 +1,66 @@
+#-----------------------------------------------------------------------------------------------------------------#
+#                                                                                                                 #
+# PROJECT: MEDICAID DRUG UTILIZATION		                                                                          #
+# AUTHOR: MICHAEL MAGUIRE, MS, DMA II                                                                             #
+# INSTITUTION: UNIVERSITY OF FLORIDA, COLLEGE OF PHARMACY                                                         #
+# DEPARTMENT: PHARMACEUTICAL OUTCOMES AND POLICY                                                                  #
+# SUPERVISORS: AMIE GOODIN, PHD, MPP | JUAN HINCAPIE-CASTILLO, PHARMD, PHD, MS                                    #
+# SCRIPT: 01_create-directories.R                                                                          			  #
+#                                                                                                                 #
+#-----------------------------------------------------------------------------------------------------------------#
+
+## Create data directory if it doesn't exist.
+
+if (dir.exists("./data")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./data")
+}
+
+## Create clean data sub-directory if it doesn't exist.
+
+if (dir.exists("./data/clean/")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./data/clean/")
+}
+
+## Create raw data sub-directory if it doesn't exist.
+
+if (dir.exists("./data/raw/")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./data/raw/")
+}
+
+## Create metadata sub-directory if it doesn't exist.
+
+if (dir.exists("./data/metadata-archive/")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./data/metadata-archive/")
+}
+
+## Create output directory if it doesn't exist.
+
+if (dir.exists("./output")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./output")
+}
+
+## Create functions directory if it doesn't exist.
+
+if (dir.exists("./functions")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./functions")
+}
+
+## Create plots directory if it doesn't exist.
+
+if (dir.exists("./plots")) {
+  print("Directory already exists!")
+} else {
+  dir.create("./plots")
+}
